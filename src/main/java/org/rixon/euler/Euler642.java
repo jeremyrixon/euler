bf137c3ad39d6f40a3ce19d9b59821f2c4ed4a1c
Jeremy Rixon <jeremy.rixon@agriculture.gov.au>
2018-11-15 13:07:35 +1100

    doing 642

--- src/main/java/org/rixon/euler/Euler642.java
+++ src/main/java/org/rixon/euler/Euler642.java
@@ -1,19 +1,31 @@
 package org.rixon.euler;
 
+import java.util.Arrays;
+
 public class Euler642 {
 	public static void main(String[] args) {
-
 		long start = System.nanoTime();
 
-//		final long N = 201820182018l;
-		final long N = 10000;
-		final long sum = N * (N + 1) / 2 - 1;
+		final int N  = 100;
+		final int N1 = N + 1;
+
+		final int[] a = new int[N1];
+		for (int i = 0; i < N1; i++) {
+			a[i] = i;
+		}
+
+		for (int i = 2; i < N1; i++) {
+			for (int j = 2 * i; j < N1; j+= i) {
+				a[j] /= a[i];
+			}
+		}
+
+		System.out.println(Arrays.toString(a));
+
+
 
 		long duration = System.nanoTime() - start;
-
 		System.out.println(duration / 1E9);
-		System.out.println(sum);
-		System.out.println(sum % 1000000000);
 	}
 
 }
